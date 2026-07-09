1d1a31eba93f6c41dd76bdf4a589b1945e3460e7
Inkydoo <shoshaniomri123@gmail.com>
2026-07-09 15:13:57 +0100

    1

--- 1_network_display.py
+++ 1_network_display.py
@@ -79,10 +79,10 @@ def display_graph():
                            directed=True,
                            physics={
                                "barnesHut": {
-                                   "gravitationalConstant": -2000,
-                                   "centralGravity": 0.05,  # pulls all nodes toward the center
-                                   "springLength": 200,  # rest length of edges
-                                   "springConstant": 0.1,  # stiffness of edges (higher = stronger pull along edges)
+                                   "gravitationalConstant": -200,
+                                   "centralGravity": 0.01,  # pulls all nodes toward the center
+                                   "springLength": 500,  # rest length of edges
+                                   "springConstant": 0.5,  # stiffness of edges (higher = stronger pull along edges)
                                    "damping": 0.09,
                                    "avoidOverlap": 0.1
                                }
